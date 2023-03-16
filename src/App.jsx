c7adb85f3bb7fe88b8823a423029e457ac07795f
Alexis Franco <108986498+Alexis196@users.noreply.github.com>
2023-03-16 17:45:24 -0300

    correciones y merge dev (#26)

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,6 @@ import { RouterProvider } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 
-import { Provider } from 'react-redux';  //enchufa estados globales a la app, todos los estados globales estaran disponibles para todos los componentes de la app
-import { store} from './store/store' // redux
-
 function App() {
   return (
     <div className="App" >
