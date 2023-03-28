40f11f873adc4f67968955e0b379c300af3ef845
Rodrigo <contactorodrigocabrera@gmail.com>
2023-03-28 18:21:07 -0300

    La redireccion a MercadoPago espera a la resolucion de la alerta

--- src/components/DonationCard/DonationCard.jsx
+++ src/components/DonationCard/DonationCard.jsx
@@ -9,14 +9,16 @@ export default function DonationCard({donation}) {
         let headers = { headers: { 'Authorization': `Bearer ${token}` } };
         try {
             await axios.post('http://localhost:8000/payment', donation, headers)
-            .then((res)=>window.location.href = res.data.response.body.init_point);
-            
-            await Swal.fire({
-                icon: 'success',
-                title: 'ÉXITO',
-                text: 'Será redirigido al link de pago'
+            .then(async (res)=>{
+                await Swal.fire({
+                    icon: 'success',
+                    title: 'Great!',
+                    text: 'Press OK. You will be redirected to MercadoPago to make the payment. Thank you so much.'
+                });
+                return Promise.resolve(res);
             })
-            } catch(error) {
+            .then((res)=>window.location.href = res.data.response.body.init_point);        
+        } catch(error) {
             let err = error.response.data.message;
             console.log('Ocurrió un error');
             await Swal.fire({
@@ -24,66 +26,15 @@ export default function DonationCard({donation}) {
                 title: 'Oops, there is a problemm!',
                 text: err
             });
-            }
         }
+    }
+    
 
     return (
         <div className='donationCard'>
             <img src={donation.image} alt="" />
-            <p> Click on the button to make a {donation.description}</p>
+            <p> Click below to make a {donation.description}</p>
             <button className='donateButton' onClick={donateAmount}>Donate ${donation.price}ARS </button>
         </div>
     )
 }
-
-
-// import React, {useEffect} from 'react'
-// import './donationCard.css'
-// import axios from 'axios'
-// import Swal from 'sweetalert2'
-
-// import { useSelector,useDispatch } from 'react-redux'
-// import getDonations from '../../store/Donations/Actions'
-
-// export default function DonationCard({donation}) {
-
-//     const dispatch = useDispatch()
-//     let token = localStorage.getItem('token');
-
-    // const donateAmount = () => {
-    //     dispatch(getDonations({token , donation }))
-    // }
-
-    // async function donateAmount() {
-    //     let token = localStorage.getItem('token');
-    //     let headers = { headers: { 'Authorization': `Bearer ${token}` } };
-    //     try {
-    //         await axios.post('http://localhost:8000/payment', donation, headers)
-    //         .then((res)=>window.location.href = res.data.response.body.init_point);
-            
-    //         // await Swal.fire({
-    //         //     icon: 'success',
-    //         //     title: 'ÉXITO',
-    //         //     text: 'Será redirigido al link de pago'
-    //         // })
-    //         } catch(error) {
-    //         let err = error.response.data.message;
-    //         console.log('Ocurrió un error');
-    //         await Swal.fire({
-    //             icon: 'error',
-    //             title: 'Oops, there is a problemm!',
-    //             text: err
-    //         });
-    //         }
-    //     }
-
-//     return (
-//         <div className='donationCard'>
-//             <img src={donation.image} alt="" />
-//             <p> Click on the button to make a {donation.description}</p>
-//             <button className='donateButton' onClick={() => {
-//         dispatch(getDonations({token , donation }))
-//     }}>Donate ${donation.price}ARS </button>
-//         </div>
-//     )
-// }
